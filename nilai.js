68d857aa3272525adb6152cf8be4f982b1907656
Ario Kusumo <arioorder2002@gmail.com>
2022-12-09 14:53:15 +0700

    full

---
+++ nilai.js
@@ -0,0 +1,18 @@
+<html>
+   <body>     
+      <script type = "text/javascript">
+         <!--
+            var nilai = 50;
+         
+            if( nilai > 60 ) {
+               document.write("<b>Bagus!</b>");
+            }
+         //-->
+      </script>      
+      <p>Nilai Ujian</p>
+   </body>
+</html>
+// If statement adalah pernyataan kontrol dasar yang memungkinkan JavaScript untuk membuat keputusan dan mengeksekusi pernyataan secara kondisional. Berikut ini adalah contoh If Statement syntax:
+// if (kondisi) {
+//    Code yang akan dieksekusi jika memenuhi kondisi
+// }
